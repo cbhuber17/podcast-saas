d4fb7e5b1ef33a6fa3dd1d0496e5e8d2b8b6ba2d
Colin Huber <cbhuber@gmail.com>
2024-06-01 21:02:04 -0600

    backend: Convex setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://modern-foal-31.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
